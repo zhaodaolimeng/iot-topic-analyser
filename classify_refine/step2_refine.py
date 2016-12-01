6165a7c97c8575be05bd8ca086a8915ceb4ff78d
zhaodaolimeng@gmail.com <zhaodaolimeng@gmail.com>
2016-12-01 17:14:32 +0800

    DONE: 可以输出每次优化后每个组的节点选择。

--- classify_refine/step2_refine.py
+++ classify_refine/step2_refine.py
@@ -63,6 +63,7 @@ class RfRefiner(object):
                 group_id.append(old_f)
                 group_p.append(np.asarray(counter) / np.sum(counter))
                 counter = [0] * self.n_types
+                counter[sensor_choice[i]] += 1
                 old_f = f
         group_id.append(old_f)
         group_p.append(np.asarray(counter) / np.sum(counter))
@@ -76,26 +77,22 @@ class RfRefiner(object):
     def run_once(self):
 
         # 执行一次聚类
-        cluster_label, mu_list = self.kmeans(self.group_p, max_iter=100)
+        cluster_label, mu_list = self.kmeans(self.group_p, max_iter=10)
 
         # 找到每个组对应的sensor的id
-        group_sensor_dict = dict()
+        group_dict = dict()
         for idx, f in enumerate(self.feedid):
-            group_sensor_dict[f] = self.sensorid[idx] \
-                if len(group_sensor_dict[f]) == 0 else group_sensor_dict[f] + self.sensorid[idx]
+            if f not in group_dict:
+                group_dict[f] = []
+            group_dict[f].append(idx)
 
-        # 每个组可计算得到一个偏差值，每个点是一个组，每个组的sensor是?
+        # 得到每个组对应的sensor的分布sensor_proba
         for idx, cluster in enumerate(cluster_label):
-            difference = self.group_p[idx] - mu_list[cluster]
+            sensor_proba = []  # 当前组每个节点的分布 n*len(features[0])
+            for sensor_idx in group_dict[self.group_id[idx]]:
 
-            sensor_proba = []
-            sensor_choice = []
-
-            for sensor_idx in group_sensor_dict[self.group_id[idx]]:
                 sensor_proba.append(self.sensor_p[sensor_idx])
-                sensor_choice.append(self.sensor_choice[sensor_idx])
-
-            self.adjust_sensor_choice(sensor_proba, sensor_choice, difference)
+            self.adjust_sensor_choice(sensor_proba, mu_list[cluster])
 
         pass
 
@@ -104,12 +101,11 @@ class RfRefiner(object):
     cluster_label为每个组属于的cluster的编号，组数目 * 1
     mu_list为每个cluster对应的坐标值，cluster数目 * sensor_choice类别数
     """
-
     @staticmethod
-    def kmeans(features, max_iter=1000):
+    def kmeans(features, n_mu=10, max_iter=10):
 
         cluster_label = [0] * len(features)
-        mu_list = np.random.rand(len(features), len(features[0]))  # 每个聚类的中心K*27
+        mu_list = np.random.rand(n_mu, len(features[0]))  # 每个聚类的中心K*27
         mu_list /= np.matrix(np.sum(mu_list, axis=1)).T
 
         for iter_times in range(max_iter):
@@ -117,16 +113,19 @@ class RfRefiner(object):
             # 对于每个点，计算到不同的mu的距离
             for i, x in enumerate(features):
                 distance = np.asarray([sum((mu - x) ** 2) for mu in mu_list])
-                cluster_label[i] = distance.argmax()
+                cluster_label[i] = distance.argmin()
 
             # 更新mu
-            mu_count = [0] * len(features)
-            mu_sum = np.zeros((len(features), len(features[0])))
+            mu_count = [0] * n_mu
+            mu_sum = np.zeros((n_mu, len(features[0])))
 
             for i, x in enumerate(features):
                 mu_sum[cluster_label[i]] += x
                 mu_count[cluster_label[i]] += 1
-            mu_list = (mu_sum.T / mu_count).T
+
+            for idx, mu_feature in enumerate(mu_list):
+                if mu_count[idx] != 0:
+                    mu_list[idx] = mu_sum[idx]/mu_count[idx]
 
         return cluster_label, mu_list
 
@@ -136,15 +135,41 @@ class RfRefiner(object):
     choice为组中每个sensor的当前选择值，组中sensor个数 * 1  
     difference为组中不同sensor选择的偏差值，sensor_choice的类别数 * 1
     输出：
-    将改写self.sensor_choice，不进行显式的输出
+    每个sensor的输出边
     """
-    def adjust_sensor_choice(self, sensor_proba, choice, difference):
+    @staticmethod
+    def adjust_sensor_choice(sensor_proba, type_target, portion_lambda=1, rounding_scale=10):
 
-        # TODO 调用nx.max_flow_min_cost求解网络流问题
         """
-        sensor_proba为
+        sensor_proba为第一层节点
+        type_target为第二层节点
+        :rtype: object
         """
+        n_sensors = len(sensor_proba)
+        graph_edge = []
+        for idx, p_si in enumerate(sensor_proba):
+            graph_edge.append((0, 1+idx, {'capacity': 1, 'weight': 0}))
+            for idx_t, _ in enumerate(type_target):
+                if p_si[idx_t] != 0:
+                    st = {'capacity': 1, 'weight': 1.0/p_si[idx_t]}
+                    graph_edge.append((1+idx, 1+idx_t+n_sensors, st))
 
+        for idx_t, t in enumerate(type_target):
+            s = sum(type_target)
+            if t != 0:
+                st = {'capacity': n_sensors, 'weight': 1.0*portion_lambda*s/t}
+                graph_edge.append((1+n_sensors + idx_t, 1 + n_sensors + len(type_target), st))
+
+        # 当weight和capacity为浮点数时方法失效，所以将权重进行rounding
+        min_weight = min([e[2]['weight'] for e in graph_edge if e[2]['weight'] > 0])
+        ratio = rounding_scale/min_weight
+        for e in graph_edge:
+            e[2]['weight'] = int(ratio*e[2]['weight'])
+
+        G = nx.DiGraph()
+        G.add_edges_from(graph_edge)
+        flow_dict = nx.max_flow_min_cost(G, 0, 1 + n_sensors + len(type_target))
+        print(flow_dict)
 
         pass
 
